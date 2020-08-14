899a35bb036a07afc427dae20879a7b8cf090393
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-14 00:24:30 +0000

    first attempt t10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" 0x02. AirBnB clone - MySQL, task 10. DBStorage - Amenity... and BOOM! """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from .base_model import BaseModel, Base
+from .place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Defines attributes for `Amenity` as it inherits from `BaseModel`,
+    and ORM properties in relation to table `amenities`.
+    Attributes:
+        name (Column): name of state, string of max 128 chars
+        amenities (relationship): many-to-many-association to `Place`
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
