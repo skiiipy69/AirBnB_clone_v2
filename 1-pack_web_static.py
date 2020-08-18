5ac591f32a33d8cb2c8d942208cc937a69e530e4
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-18 05:31:08 +0000

    first attempt t1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 0x03. AirBnB clone - Deploy static, task 1. Compress before sending
+"""
+from fabric.api import local, env
+from os import path, makedirs, listdir
+from datetime import datetime
+
+env.hosts = ['localhost']
+
+
+def do_pack():
+    """ Generates a .tgz archive from the contents `web_static/` in AirBnB clone
+    repo.
+
+    Retruns:
+        (str): full path from current directory to `.tgz` archive created in
+    `versions/`
+    """
+    if not path.isdir("./versions"):
+        makedirs("./versions")
+
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    local('tar -cvzf versions/web_static_{}.tgz web_static/'.format(now))
+
+    files = listdir("./versions")
+    paths = [path.join("./versions", base_name) for base_name in files]
+    if len(paths) == 0:
+        return None
+    return max(paths, key=path.getctime)
