b086806d342aca1d1ef10270351ac8cda64cf68b
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-02 17:48:50 +0000

    first attempt t10

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""0x04. AirBnB clone - Web framework, task 10. States and State
+"""
+from flask import Flask, render_template
+from os import environ
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+environ['FLASK_ENV'] = 'development'
+
+
+@app.teardown_appcontext
+def states_list_teardown(self):
+    """ Ensures SQLAlchemy session opened to serve dynamic content for HTML
+    templates is closed after serving.
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def cities_by_states(id=None):
+    """ Requests list of `State`s ordered by name, which populates HTML
+    template served to '/cities_by_states'.
+    """
+    return render_template('9-states.html', id=id,
+                           states=storage.all(State))
+
+
+app.run()
