c46b7c3260bf01242f44d9cb6c189aef30ac4826
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-02 06:25:03 +0000

    first attempt t8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""0x04. AirBnB clone - Web framework, task 6. Odd or even?
+"""
+from flask import Flask, render_template
+from os import environ
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+environ['FLASK_ENV'] = 'development'
+
+
+@app.teardown_appcontext
+def states_list_teardown(self):
+    """ Ensures SQLAlchemy session opened to serve dynamic content for HTML
+    templates is closed after serving.
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Requests list of `State`s ordered by name, which populates HTML
+    template served to '/states_list'.
+    """
+    session = storage._DBStorage__session
+    states_sorted = session.query(State).order_by(State.name).all()
+    return render_template('7-states_list.html', states=states_sorted)
+
+
+app.run()
